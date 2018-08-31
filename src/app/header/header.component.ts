efde69c56b6035f2f3a3eed6da9fe382730d6819
Sanjay Namdeo <sanjay.namdeo@live.com>
2018-08-31 21:44:21 +0200

    Navigation menu handler

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import {Component} from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() onMenuChangeHandler = new EventEmitter<string>();
+
+  onMenuClicked(selectedMenu: string) {
+    this.onMenuChangeHandler.emit(selectedMenu);
+  }
 
 }
