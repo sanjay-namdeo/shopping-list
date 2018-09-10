4db4dc9528aa774e8d02f71afaa3b3986c4630bc
Sanjay NAMDEO <sanjay.namdeo0ext@alstomgroup.com>
2018-09-10 23:03:53 +0200

    Added RecipeService to get list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from './../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
+  providers: [RecipeService]
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a text',
-      'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg'),
-    new Recipe('A Another Test Recipe', 'This is simply a text',
-      'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg')
-  ];
-  constructor() { }
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getReceips();
   }
 
   selectedRecipe(recipe: Recipe) {
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a text',
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg'),
+        new Recipe('A Another Test Recipe', 'This is simply a text',
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg')
+    ];
+
+    getReceips() {
+        return this.recipes.slice();
+    }
+}
