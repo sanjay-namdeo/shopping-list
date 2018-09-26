4bd47c2f31b380785ab64ef9abac0750211def65
Sanjay NAMDEO <sanjay.namdeo0ext@alstomgroup.com>
2018-09-26 22:08:38 +0200

    Added ingredients details in recipe detail section

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,14 @@
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a text',
-            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a text',
-            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg')
+        new Recipe('A Test Recipe', 'Delicious one',
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg',
+            [new Ingredient('Apple', 1), new Ingredient('Mango', 2)]),
+        new Recipe('Another Test Recipe', 'Tasty',
+            'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg',
+            [new Ingredient('Tomato', 3), new Ingredient('Potato', 2)])
     ];
 
     getReceips() {
